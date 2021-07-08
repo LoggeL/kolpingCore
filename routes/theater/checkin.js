8927d2c13b7ccf511b0fe5ae58e1f8c04d1e7345
LoggeL <hyper.xjo@gmx.de>
2021-07-08 02:18:03 +0200

    forgot await >_<

--- routes/theater/checkin.js
+++ routes/theater/checkin.js
@@ -7,8 +7,7 @@ module.exports = (app, db) => {
         const registration = await db('registration').where('token', token).leftJoin('event', 'event.id', 'registration.event_id').select('registration.name', 'registration.surname', 'registration.people_count', 'event.date', 'event.name as eventname')
         if (registration.legth == 0) return res.status(404).json({ error: "Registrierung nicht gefunden!" })
 
-        const checkedIn = db('checkin').where('token', token).select('date')
-        console.log(checkedIn)
+        const checkedIn = await db('checkin').where('token', token).select('date')
         if (checkedIn.length > 0) return res.status(400).json({ error: "Bereits eingecheckt (" + new Date(checkedIn[0].date).toLocaleDateString() + ")" })
 
         db('checkin').insert({ token, date: Date.now() })
