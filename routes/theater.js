46daa32f12d856f3943573a05930d99680df7d51
LoggeL <hyper.xjo@gmx.de>
2021-07-08 02:07:46 +0200

    add checkinRoute

--- routes/theater.js
+++ routes/theater.js
@@ -2,4 +2,5 @@ module.exports = (app, db) => {
     require('./theater/registration.js')(app, db)
     require('./theater/event.js')(app, db)
     require('./theater/ticket.js')(app, db)
+    require('./theater/checkin.js')(app, db)
 }
---
+++ routes/theater/checkin.js
@@ -0,0 +1,15 @@
+const { checkIn_secret } = require('./secrets.json')
+
+module.exports = (app, db) => {
+    app.post('/api/public/theater/checkin', async (req, res) => {
+        const { token, checkIn } = req.body
+        if (checkIn != checkIn_secret) res.status(403).json({ error: "Falscher Key" })
+        const registration = await db('registration').where('token', token).leftJoin('event', 'event.id', 'registration.event_id').select('registration.name', 'registration.surname', 'registration.people_count', 'event.date', 'event.name as eventname')
+        if (registration.legth == 0) return res.status(404).json({ error: "Registrierung nicht gefunden!" })
+
+        db('checkin').insert({ token, date: Date.now() })
+        console.log('checkIn', token)
+
+        return res.status(200).json(registration[0])
+    })
+}
