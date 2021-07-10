5d643bc398a7d23d84a2ee92a5fc70aaa33a8a11
LoggeL <hyper.xjo@gmx.de>
2021-07-11 00:11:46 +0200

    mark ticket after checkin

--- routes/theater/ticket.js
+++ routes/theater/ticket.js
@@ -12,6 +12,8 @@ module.exports = async (app, db) => {
         const qr_data = await db('registration').where('token', token).leftJoin('event', 'event.id', 'registration.event_id').select('date', 'people_count')
         if (qr_data.length == 0) return res.status(404).json({ error: "Anmeldung nicht gefunden" })
 
+        const checkin_data = await db('checkin').where('token', token).select('date')
+
         const RQ_URL = await QRCode.toDataURL("http://theater.kolping-ramsen.de/ticket.html?" + token, { margin: 0 })
 
         const QR_SVG_EDIT = QR_SVG
@@ -21,6 +23,10 @@ module.exports = async (app, db) => {
             .replace('{{DATE}}', fns.format(new Date(qr_data[0].date), 'dd/MM/yyyy | HH:mm'))
             .replace('{{TOKEN}}', token)
             .replace('{{QR_URL}}', RQ_URL)
+            .replace('{{EINGECHECKT}}', checkin_data[0] ? 'Eingecheckt' : '')
+            .replace('{{EINGECHECKT_DATE}}', checkin_data[0] ? fns.format(new Date(checkin_data[0].date), 'dd/MM/yyyy HH:mm') : '')
+            .replace('{{EINGECHECKT_WIDTH}}', checkin_data[0] ? '100%' : '0%')
+
 
         res.setHeader('content-type', 'image/svg+xml')
         return res.status(200).send(QR_SVG_EDIT)
