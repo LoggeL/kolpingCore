99a46a829dae4d9bb95e5c334e6348acaad100c2
LoggeL <hyper.xjo@gmx.de>
2021-07-14 01:58:19 +0200

    checked in ticket version

--- routes/theater/ticket.js
+++ routes/theater/ticket.js
@@ -3,6 +3,7 @@ const fns = require('date-fns')
 const QRCode = require('qrcode')
 
 const QR_SVG = fs.readFileSync('routes/theater/ticket.svg', 'utf-8')
+const QR_SVG_checkin = fs.readFileSync('routes/theater/ticket_checkin.svg', 'utf-8')
 
 module.exports = async (app, db) => {
     app.get('/api/public/theater/ticket/:token', async (req, res) => {
@@ -16,7 +17,7 @@ module.exports = async (app, db) => {
 
         const RQ_URL = await QRCode.toDataURL("http://theater.kolping-ramsen.de/ticket.html?" + token, { margin: 0 })
 
-        const QR_SVG_EDIT = QR_SVG
+        const QR_SVG_EDIT = (checkin_data[0] ? QR_SVG_checkin : QR_SVG)
             .replace('{{PERSON}}', qr_data[0].people_count == 1 ? 'PERSON' : 'PERSONEN')
             .replace('{{PERSON_X}}', qr_data[0].people_count == 1 ? '970' : '940')
             .replace('{{PEOPLE_COUNT}}', qr_data[0].people_count)
@@ -25,7 +26,6 @@ module.exports = async (app, db) => {
             .replace('{{QR_URL}}', RQ_URL)
             .replace('{{EINGECHECKT}}', checkin_data[0] ? 'Eingecheckt' : '')
             .replace('{{EINGECHECKT_DATE}}', checkin_data[0] ? fns.format(new Date(checkin_data[0].date), 'dd/MM/yyyy HH:mm') : '')
-            .replace('{{EINGECHECKT_WIDTH}}', checkin_data[0] ? '100%' : '0%')
 
 
         res.setHeader('content-type', 'image/svg+xml')
