80f985400b2497d8ba8df83e2de2f83ba2da8a45
LoggeL <hyper.xjo@gmx.de>
2021-08-15 23:34:04 +0200

    add scan endpoint

--- routes/theater/checkin.js
+++ routes/theater/checkin.js
@@ -5,6 +5,7 @@ module.exports = (app, db) => {
     app.post('/api/public/theater/checkin', async (req, res) => {
         const { token, checkIn } = req.body
         if (checkIn != checkIn_secret) return res.status(403).json({ error: "Falscher Key" })
+        if (!token) return res.status(403).json({ error: "Kein Token" })
         const registration = await db('registration').where('token', token).leftJoin('event', 'event.id', 'registration.event_id').select('registration.name', 'registration.surname', 'registration.people_count', 'event.date', 'event.name as eventname')
         if (registration.legth == 0) return res.status(404).json({ error: "Registrierung nicht gefunden!" })
 
--- scanWebview/js/main.js
+++ scanWebview/js/main.js
@@ -1,8 +1,39 @@
-QrScanner.WORKER_PATH = 'js/qr-scanner-worker.min.js'
-const videoElem = document.getElementById('scannerArea')
-const qrScanner = new QrScanner(videoElem, result => {
-    qrScanner.stop()
-    console.log('decoded qr code:', result)
-    alert(result)
+const key = document.getElementById('key').value
+fetch('/api/public/theater/checkin', {
+    method: 'POST',
+    body: new URLSearchParams(jsonToQS({ checkIn: key })),
+}).then(response => response.json()).then(json => {
+    if (json.error === "Kein Token") {
+        console.log('Check')
+        // Dis good
+        QrScanner.WORKER_PATH = 'js/qr-scanner-worker.min.js'
+        const videoElem = document.getElementById('scannerArea')
+        const qrScanner = new QrScanner(videoElem, result => {
+            if (!result.startsWith('http://theater.kolping-ramsen.de/ticket.html?')) return alert('Ungültiges Code Format')
+            const token = result.replace('http://theater.kolping-ramsen.de/ticket.html?', '')
+            qrScanner.stop()
+            console.log('decoded qr code:', result)
+            fetch('/api/public/theater/checkin', {
+                method: 'POST',
+                body: new URLSearchParams(jsonToQS({ checkIn: key, token })),
+            }).then(response => response.json()).then(json => {
+                if (json.error) {
+                    return alert(json.error)
+                }
+                alert(`${json.eventname}\n${json.name} ${json.surname}\nAnzahl: ${json.people_count}`)
+                setTimeout(() => qrScanner.start(), 1000)
+            })
+
+        })
+        qrScanner.start()
+
+    }
 })
-qrScanner.start()
+
+function jsonToQS(json) {
+    var qs = []
+    for (element in json) {
+        qs.push(element + "=" + json[element])
+    }
+    return "?" + qs.join("&")
+}
